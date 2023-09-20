649b6bbdce4465241ba332dc1cdf16154b55bcf9
KenanaTurabi <kenanaturabi@gmail.com>
2023-09-20 09:44:26 +0300

    creating bots classes (rain bot, snow bot and sun bot)

---
+++ Bots/RainBot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringAndReportingSystem.Bots
+{
+    internal class RainBot
+    {
+        public int HumidityThreshold { get; set; }
+        public string Message { get; set; } 
+        public RainBot(int humidityThreshold, string message)
+        {
+            HumidityThreshold = humidityThreshold;
+            Message = message;
+        }
+        public void Activate(int Humidity)
+        {
+            if (Humidity > HumidityThreshold)
+                Console.WriteLine($"RainBot activated!\r\nRainBot: \"{Message}\"");
+        }
+    }
+}
