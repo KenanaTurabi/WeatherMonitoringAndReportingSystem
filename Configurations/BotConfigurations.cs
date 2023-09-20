d08bb4c067c1aee74553d5f49983afd3ecc948a8
KenanaTurabi <kenanaturabi@gmail.com>
2023-09-20 09:49:16 +0300

    adding config.json file and creating configuration classes to read from config.json  and set the properties for each bot class

---
+++ Configurations/BotConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringAndReportingSystem.Configurations
+{
+    internal class BotConfigurations
+    {
+
+        
+            public RainBotConfiguration RainBotConfiguration { get; set; }
+            public SunBotConfiguration SunBotConfiguration { get; set; }
+            public SnowBotConfiguration SnowBotConfiguration { get; set; }
+        
+
+    }
+
+
+}
---
+++ Configurations/SnowBotConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringAndReportingSystem.Configurations
+{
+    internal class SnowBotConfiguration
+    {
+        public int temperatureThreshold { get; set; }
+        public bool enabled { get; set; }
+        public string message { get; set; }
+    }
+}
