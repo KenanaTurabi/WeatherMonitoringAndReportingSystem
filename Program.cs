5c6c0815ea0c90fcadf1847d31cc81b68323160c
KenanaTurabi <kenanaturabi@gmail.com>
2023-09-27 13:42:59 +0300

    applying Factory design pattern

--- Program.cs
+++ Program.cs
@@ -1,62 +1,84 @@
-﻿using WeatherMonitoringAndReportingSystem.Bots;
+﻿using System.Xml.Linq;
+using WeatherMonitoringAndReportingSystem.Bots;
 using WeatherMonitoringAndReportingSystem.Configurations;
 using WeatherMonitoringAndReportingSystem.Models;
+using System.Xml;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace WeatherMonitoringAndReportingSystem
 {
     internal class Program
     {
+        public static bool IsJson(string data)
+        {
+            try
+            {
+                JToken.Parse(data);
+                return true;    
+            }
+            catch (JsonReaderException)
+            {
+                return false;
+            }
+        }
+        public static bool IsXml(string data)
+        {
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(data);
+                return true;
+            }
+            catch (XmlException)
+            {
+                return false;
+            }
+        }
         static void Main(string[] args)
         {
-            
+            IHandler Handler;
+            FormatFactory formatFactory=new FormatFactory();
             BotConfigurations configurations = ConfigurationsReader.ReadFromJsonFile();
             if (configurations != null)
             {
                 RainBot rainBot = new RainBot(configurations.RainBotConfiguration.humidityThreshold,configurations.RainBotConfiguration.message);
                 SnowBot snowBot = new SnowBot(configurations.SnowBotConfiguration.temperatureThreshold, configurations.SnowBotConfiguration.message);
                 SunBot sunBot = new SunBot(configurations.SunBotConfiguration.temperatureThreshold, configurations.SunBotConfiguration.message);
-                Console.Write("Do you want to enter xml format or json format? ");
-                string XmlOrJson = Console.ReadLine();
-                if (string.Equals(XmlOrJson, "xml", StringComparison.OrdinalIgnoreCase))
+                Console.Write("plz enter data in xml format or json format: ");
+                string data = Console.ReadLine();
+                //<WeatherData><Location>City Name</Location><Temperature>32</Temperature><Humidity>40</Humidity></WeatherData>
+                //{"Location": "City Name", "Temperature": 32, "Humidity": 40}
+                string type;
+                if (IsJson(data))
                 {
-                    //<WeatherData><Location>City Name</Location><Temperature>32</Temperature><Humidity>40</Humidity></WeatherData>
-                    Console.Write("Enter data in xml format:  ");
-                    string xmlData = Console.ReadLine();
-                    XmlFormatHandler xmlHandler = new XmlFormatHandler();
-                    WeatherData weatherFromXml = xmlHandler.ParseXml(xmlData);
-                    rainBot.Activate(weatherFromXml.Humidity);
-                    snowBot.Activate(weatherFromXml.Temperature);
-                    sunBot.Activate(weatherFromXml.Temperature);
-
+                    Console.WriteLine("The data is in JSON format.");
+                    type = "json";
                 }
-
-                else if (string.Equals(XmlOrJson, "json", StringComparison.OrdinalIgnoreCase))
+                else if (IsXml(data))
                 {
-                    //{"Location": "City Name", "Temperature": 32, "Humidity": 40}
-                    Console.Write("Enter data in json format:  ");
-                    string jsonData = Console.ReadLine();
-                    JsonFormatHandler jsonHandler = new JsonFormatHandler();
-                    WeatherData weatherFromJson = jsonHandler.ParseJson(jsonData);
-                    rainBot.Activate(weatherFromJson.Humidity);
-                    snowBot.Activate(weatherFromJson.Temperature);
-                    sunBot.Activate(weatherFromJson.Temperature);
+                    Console.WriteLine("The data is in XML format.");
+                    type = "Xml";
+
                 }
                 else
                 {
+                    Console.WriteLine("The data is in Unkown format.");
+                    type = "Unkown";
+                }
+                if (formatFactory.getFormateType(type) == null)
+                {
                     Console.WriteLine("invalid format");
                 }
-
-                
-
-                
-
-                
-
+                else
+                {
+                    Handler = formatFactory.getFormateType(type);
+                    WeatherData weatherData = Handler.Parse(data);
+                    rainBot.Activate(weatherData.Humidity);
+                    snowBot.Activate(weatherData.Temperature);
+                    sunBot.Activate(weatherData.Temperature);
+                }
             }
-
-
-
-
         }
     }
 }
